5bc925f02b71b2f7103d97621ff0844578904d75
ShrutiVaghela <shruti141005@gmail.com>
2026-03-11 22:57:45 +0530

    Added Firebase config and installed library

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9weaDjXdV8sAIkPGBtpY7UAA1KYeuoUY",
+  authDomain: "prepassistant-ai.firebaseapp.com",
+  projectId: "prepassistant-ai",
+  storageBucket: "prepassistant-ai.firebasestorage.app",
+  messagingSenderId: "51588621184",
+  appId: "1:51588621184:web:78924ada57c9b4026cd765"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
